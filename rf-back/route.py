1c5c6b5181a6c490f675a1c2d9d064de53a63eae
koo <yangha93@naver.com>
2018-11-24 17:54:13 +0900

    first

--- rf-back/route.py
+++ rf-back/route.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 from flask_cors import CORS
 from flask import Flask, render_template, request, Response, redirect
 
+
 app = Flask(__name__)
 CORS(app)
 
@@ -21,14 +22,13 @@ def hello_world():
 #예산, 이미지 제출 post
 @app.route('/', methods=['POST'])
 def upload_file():
-    #BUDGET = int(request.form['budget']) # 전체 예산 전역변수
-    #print(BUDGET)
-    file = request.files['image']
-    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)    
+    file = request.files['image']    
+    # 확장자 명 추출
+    extension = file.filename.rsplit('.', 1)[1].lower()
+    f = os.path.join(app.config['UPLOAD_FOLDER'], 'uploaded.'+extension)    
     
     file.save(f)
     return render_template('index.html')
-
 
 # naver api 예시로 사용
 @app.route("/apiTest")
@@ -67,7 +67,6 @@ def apiTestPost() :
       return redirect('/', code=302) 
   else:
       print("Error Code:" + rescode)
-
 
 if __name__ == '__main__' :
   app.run(debug = True, host="0.0.0.0", port=int(os.getenv('VCAP_APP_PORT', '10000')))
