e55cfc8695c95d396c76cfbe96bfed073e876cee
he1ght <dkcla0223@gmail.com>
2018-11-28 17:56:45 +0900

    change type to dict add link & title

--- Similarity/check_similarity.py
+++ Similarity/check_similarity.py
@@ -18,12 +18,12 @@ def img_sim(input_path, target_list, threshold=0.7, use_gpu=True):
     for file in target_list:
         # filename = os.fsdecode(file)
         try:
-            img = Image.open(file)
+            img = Image.open(file['img'])
         except OSError:
-            img = Image.open(requests.get(file, stream=True).raw)
+            img = Image.open(requests.get(file['img'], stream=True).raw)
         img = img.convert("RGB")
         vec = img2vec.get_vec(img)
-        pics[file] = vec
+        pics[file['img']] = vec
     result = []
     try:
         sims = {}
@@ -32,9 +32,17 @@ def img_sim(input_path, target_list, threshold=0.7, use_gpu=True):
 
         d_view = [(v, k) for k, v in sims.items()]
         # d_view.sort(reverse=True)
-        for v, k in d_view:
+        for (v, k), file in zip(d_view, target_list):
             # print("{:10s} : {} [{}]".format(k, 'O' if v >= threshold else 'X', v))
-            result.append({'img': k, 'sim': v, 'result': v >= threshold})
+            assert k == file['img']
+            result.append({'img': k,
+                           'sim': v,
+                           'result': v >= threshold,
+                           'lp': file['lp'],
+                           'hp': file['hp'],
+                           'link': file['link'],
+                           'title': file['title']
+                           })
 
     except KeyError as e:
         print('Could not find filename %s' % e)
@@ -46,5 +54,7 @@ def img_sim(input_path, target_list, threshold=0.7, use_gpu=True):
 
 if __name__ == "__main__":
     result = img_sim(input_path="https://shopping-phinf.pstatic.net/main_1547247/15472472225.20180927155901.jpg",
-                     target_list=["./example/test/sim_test1.JPG", "./example/test/sim_test2.jpg", "./example/test/sim_test3.jpeg"])
+                     target_list=[{'img': "./example/test/sim_test1.JPG", 'lp': 5, 'hp': 0, 'link': ".", 'title': "1"},
+                                  {'img': "./example/test/sim_test2.jpg", 'lp': 0, 'hp': 100, 'link': ".", 'title': "1"},
+                                  {'img': "./example/test/sim_test3.jpeg", 'lp': 10, 'hp': 300, 'link': ".", 'title': "1"}])
     print(result)
